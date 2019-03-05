065ec0cabd74061b560bb846c8b4da663f960d56
Schubert <erik.schubert@sap.com>
2019-03-05 16:04:28 +0100

    Better messaging

--- appartement/apartment.go
+++ appartement/apartment.go
@@ -12,6 +12,7 @@ import (
 )
 
 func main() {
+	fmt.Println(calcConsumption(time.Now()))
 	conn, err := amqp.Dial("amqp://KVK-T3NDRHs9k9eF:fz9CGA1xCu3zqWCK@10.11.241.45:49977/")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -20,26 +21,23 @@ func main() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	_, err = channel.QueueDeclare("producer", true, false, false, false, nil)
+	_, err = channel.QueueDeclare("apartment", true, false, false, false, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 	background(channel)
 }
 
-func calcOutput(basePower float64, t time.Time) float64 {
+func calcConsumption(t time.Time) float64 {
 	h, m, s := t.Clock()
 	secs := h*3600 + m*60 + s
-	if secs < 21600 || secs > 64800 {
-		return 0
-	}
-	return basePower * -math.Cos(float64(secs)/86400.0*math.Pi*2.0)
+	return 20 * -math.Sin(float64(secs)/86400.0*math.Pi*2.0)
 }
 
-func writeProduction(id uint32, prod float64, ts time.Time) []byte {
+func writeConsumption(id uint32, consumption float64, ts time.Time) []byte {
 	buf := make([]byte, 20)
 	binary.BigEndian.PutUint32(buf[:4], id)
-	binary.BigEndian.PutUint64(buf[4:12], math.Float64bits(prod))
+	binary.BigEndian.PutUint64(buf[4:12], math.Float64bits(consumption))
 	binary.BigEndian.PutUint64(buf[12:20], uint64(ts.UnixNano()))
 	return buf
 }
@@ -53,8 +51,8 @@ func background(channel *amqp.Channel) {
 
 	for ts := range calcTicker.C {
 		for i := 0; i < 4; i++ {
-			msg := writeProduction(uint32(i), calcOutput(float64(i*5+5), ts), ts)
-			err := (*channel).Publish("", "producer", false, false, amqp.Publishing{
+			msg := writeConsumption(uint32(i), calcConsumption(ts), ts)
+			err := (*channel).Publish("", "apartment", false, false, amqp.Publishing{
 				Headers:         amqp.Table{},
 				ContentType:     "text/plain",
 				ContentEncoding: "",
